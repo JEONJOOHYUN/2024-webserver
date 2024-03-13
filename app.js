63e6c064a69b672fc7c746f5e8daa71e93efc75c
eunseok <eunseok@cedar.kr>
2024-03-13 11:54:31 +0900

    js 수정완료

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express();
 // 기본 포트를 설정하거나 3000 포트를 사용합니다.
 const PORT = process.env.PORT || 3000;
 
-
+app.use('/public', express)
 
 // 루트 경로 ('/')에 대한 GET 요청을 처리합니다.
 app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+  res.send('Hello, Express.js!!!');
 });
 
 app.get('/test', (req, res) => {
